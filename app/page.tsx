6a105eb0340b26077d1de96a23181cf760f3152e
savio1233 <savioabraham0@gmail.com>
2024-12-05 16:38:54 +0530

    Changes have been made

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,12 @@ export default function Home() {
   return (
     <div className="flex h-screen">
       {/* Left Section - Blue */}
-      <div className="w-1/3 bg-blue-500 text-white flex flex-col items-center justify-start p-6">
+      <div className="background-center" style={{ backgroundImage: 'url(Background_-_49.jpg)' }}>
         <Header />
       </div>
 
       {/* Right Section - White */}
-      <div className="w-2/3 bg-white p-6 overflow-auto">
+      <div className="w-2/2 bg-white p-6 overflow-auto">
         <AboutSection />
       </div>
     </div>
